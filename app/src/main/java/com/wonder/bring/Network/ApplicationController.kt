ad0e033e4d35601f6d8d5c523aec6eed235e4fc8
Limkyuhee <kyuhee2118@gmail.com>
2018-12-24 01:26:30 +0900

    first commit

---
+++ app/src/main/java/com/wonder/bring/Network/ApplicationController.kt
@@ -0,0 +1,30 @@
+package com.wonder.bring.Network
+
+import android.app.Application
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class ApplicationController : Application() {
+
+    private val baseURL="wonder server"
+    lateinit var networkService: NetworkService
+
+    companion object {
+        lateinit var instance: ApplicationController
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance=this   // 싱글톤으로 만든 instance변수에 본인이 들어가고,
+        buildNetwork()
+    }
+
+    fun buildNetwork(){
+        val retrofit:Retrofit=Retrofit.Builder()
+            .baseUrl(baseURL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        networkService=retrofit.create(NetworkService::class.java)          //
+    }
+}
---
+++ app/src/main/java/com/wonder/bring/Network/NetworkService.kt
@@ -0,0 +1,12 @@
+package com.wonder.bring.Network
+
+import com.google.gson.JsonObject
+import retrofit2.Call
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.http.*
+
+interface NetworkService {
+
+
+}
