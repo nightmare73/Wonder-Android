8bc3884ebc4633c0e890b0742a72e6415aeb646b
Limkyuhee <kyuhee2118@gmail.com>
2018-12-28 00:55:54 +0900

    backpressed add

--- app/src/main/java/com/wonder/bring/MainActivity.kt
+++ app/src/main/java/com/wonder/bring/MainActivity.kt
@@ -7,8 +7,12 @@ import android.widget.RelativeLayout
 import com.wonder.bring.MainFragment.MainFragmentViewPager
 import com.wonder.bring.MainFragment.MyFragmentStatePagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
+
+    // backpressed변수
+    var backPressedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,20 +21,38 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    // 탭바 추가 함수
     private fun configureBottomNavigation() {
 
         // 뷰페이저 스와이프 막기 위해서, customviewpager사용
-        val customViewPager =findViewById(R.id.vp_bottom_main_act_frag_pager) as MainFragmentViewPager
+        val customViewPager = findViewById(R.id.vp_bottom_main_act_frag_pager) as MainFragmentViewPager
         customViewPager.setPagingEnabled(false)
-        customViewPager.adapter=MyFragmentStatePagerAdapter(supportFragmentManager,4)
+        customViewPager.adapter = MyFragmentStatePagerAdapter(supportFragmentManager, 4)
 
         tl_bottom_main_act_bottom_menu.setupWithViewPager(vp_bottom_main_act_frag_pager)
 
         val bottomNaviLayout: View = this.layoutInflater.inflate(R.layout.bottom_navigation_tab, null, false)
 
-        tl_bottom_main_act_bottom_menu.getTabAt(0)!!.customView = bottomNaviLayout.findViewById(R.id.btn_bottom_navi_main_tab) as RelativeLayout
-        tl_bottom_main_act_bottom_menu.getTabAt(1)!!.customView = bottomNaviLayout.findViewById(R.id.btn_bottom_navi_map_tab) as RelativeLayout
-        tl_bottom_main_act_bottom_menu.getTabAt(2)!!.customView = bottomNaviLayout.findViewById(R.id.btn_bottom_navi_my_page_tab) as RelativeLayout
-        tl_bottom_main_act_bottom_menu.getTabAt(3)!!.customView=bottomNaviLayout.findViewById(R.id.btn_bottom_navi_cart_tab) as RelativeLayout
+        tl_bottom_main_act_bottom_menu.getTabAt(0)!!.customView =
+                bottomNaviLayout.findViewById(R.id.btn_bottom_navi_main_tab) as RelativeLayout
+        tl_bottom_main_act_bottom_menu.getTabAt(1)!!.customView =
+                bottomNaviLayout.findViewById(R.id.btn_bottom_navi_map_tab) as RelativeLayout
+        tl_bottom_main_act_bottom_menu.getTabAt(2)!!.customView =
+                bottomNaviLayout.findViewById(R.id.btn_bottom_navi_my_page_tab) as RelativeLayout
+        tl_bottom_main_act_bottom_menu.getTabAt(3)!!.customView =
+                bottomNaviLayout.findViewById(R.id.btn_bottom_navi_cart_tab) as RelativeLayout
+    }
+
+    override fun onBackPressed() {
+        var temp: Long = System.currentTimeMillis()
+        var intervalTime: Long = temp - backPressedTime
+        if (intervalTime in 0..2000) {
+            super.onBackPressed()
+        } else {
+            backPressedTime = temp
+            toast("버튼을 한번 더 누르면 종료됩니다.")
+        }
     }
 }
+
+
