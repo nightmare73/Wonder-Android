a0b245f2aef9faff94ac4c2c5d123ce5ed5f8ebf
Malibin <nightmare73@naver.com>
2019-01-09 20:03:04 +0900

    order history detail View added

---
+++ app/src/main/java/com/wonder/bring/OrderHistoryDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.wonder.bring
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OrderHistoryDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_history_detail)
+    }
+}
