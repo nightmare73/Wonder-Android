1a2a66d0de8c360819c4d482b51b344dd1c82f31
Malibin <nightmare73@naver.com>
2019-01-11 00:35:26 +0900

    로그인, 회원가입 개선됨

--- app/src/main/java/com/wonder/bring/LoginProcess/LoginActivity.kt
+++ app/src/main/java/com/wonder/bring/LoginProcess/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.wonder.bring.LoginProcess
 
 import android.content.Intent
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -17,10 +18,11 @@ import com.wonder.bring.Network.ApplicationController
 import com.wonder.bring.Network.NetworkService
 import com.wonder.bring.Network.Post.PostLogInResponse
 import com.wonder.bring.R
+import com.wonder.bring.R.id.btn_login_act_login
+import com.wonder.bring.R.id.et_login_act_pw
 import com.wonder.bring.db.SharedPreferenceController
 import kotlinx.android.synthetic.main.activity_login.*
-import org.jetbrains.anko.startActivity
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.*
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
@@ -28,14 +30,16 @@ import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
 
-    var input_id : String = ""
-    var input_pw : String = ""
+    var input_id: String = ""
+    var input_pw: String = ""
+
+
 
     // backpressed변수
     var backPressedTime: Long = 0
 
     // 통신을 위한 변수
-    val networkService : NetworkService by lazy {
+    val networkService: NetworkService by lazy {
         ApplicationController.instance.networkService
     }
 
@@ -44,45 +48,14 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
 
-        setOnBtnClickListner()
+        viewInit()
 
-        // edittext입력 시, 로그인버튼 색깔 변하게 하기   Q. 색이 안변했을땐 아예 클릭이 안되게 변경해야할듯, 그리고 로그인이 회색일땐 넘어가지 않도록.
-        et_login_act_pw.addTextChangedListener(object : TextWatcher{
-
-            override fun afterTextChanged(p0: Editable?) {
-                input_id = et_login_act_id.text.toString()
-                input_pw = et_login_act_pw.text.toString()
-
-                if(input_id.length > 0 && input_pw.length > 0)
-                    btn_login_act_login.isSelected=true
-                else
-                    btn_login_act_login.isSelected=false
-
-            }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            }
-        })
 
     }
 
+    private fun viewInit() {
 
-
-
-    private fun setOnBtnClickListner(){
-
-        // 로그인
-        btn_login_act_login.setOnClickListener {
-
-            if(input_id.length > 0 && input_pw.length > 0)
-                getLoginResponse()
-            else
-                toast("정보를 모두 입력해주세요")
-
-        }
+        btn_login_act_login.isEnabled = false
 
         // 회원가입으로 이동
         tv_login_act_movetosignup.setOnClickListener {
@@ -92,36 +65,82 @@ class LoginActivity : AppCompatActivity() {
         // 뒤로가기
         btn_login_act_back.setOnClickListener {
             finish()
-//            overridePendingTransition(R.anim.slide_in_down,0)
         }
 
+        et_login_act_id.addTextChangedListener(
+            object : TextWatcher {
+                override fun afterTextChanged(p0: Editable?) {
+                    checkEmpty()
+                }
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+            })
+
+        et_login_act_pw.addTextChangedListener(
+            object : TextWatcher {
+                override fun afterTextChanged(p0: Editable?) {
+                    checkEmpty()
+                }
+
+                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+
+                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                }
+            })
+
+        btn_login_act_login.setOnClickListener {
+            getLoginResponse()
+            Log.v("malibin debug","로그인버튼눌림")
+        }
 
     }
 
+    fun checkEmpty() {
+        if (et_login_act_id.text.isNotEmpty() && et_login_act_pw.text.isNotEmpty()) {
+            btn_login_act_login.backgroundResource = R.color.salmon
+            tv_logintext.textColor = Color.parseColor("#FFFFFF")
+            btn_login_act_login.isEnabled = true
+        } else {
+            btn_login_act_login.backgroundResource = R.color.white
+            tv_logintext.textColor = Color.parseColor("#909090")
+            btn_login_act_login.isEnabled = false
+        }
+
+        input_id = et_login_act_id.text.toString()
+        input_pw = et_login_act_pw.text.toString()
+    }
+
     // 로그인 통신 로직
-    private fun getLoginResponse(){
-        if(input_id.isNotEmpty()&&input_pw.isNotEmpty()){
+    private fun getLoginResponse() {
+        if (input_id.isNotEmpty() && input_pw.isNotEmpty()) {
 
 
             //json형식의 객체 만들기
-            var jsonObject= JSONObject()
-            jsonObject.put("id",input_id)
-            jsonObject.put("password",input_pw)
+            var jsonObject = JSONObject()
+            jsonObject.put("id", input_id)
+            jsonObject.put("password", input_pw)
 
             //Gson라이브러리의 json parser를 통해 json으로!
             val gsonObject = JsonParser().parse(jsonObject.toString()) as JsonObject
 
-            Log.d("GsonObject",gsonObject.toString())
+            Log.d("GsonObject", gsonObject.toString())
 
             //통신 시작
-            val postLoginResponse : Call<PostLogInResponse> = networkService.postLoginResponse("application/json",gsonObject)
+            val postLoginResponse: Call<PostLogInResponse> =
+                networkService.postLoginResponse("application/json", gsonObject)
             postLoginResponse.enqueue(object : Callback<PostLogInResponse> {
                 override fun onResponse(call: Call<PostLogInResponse>, response: Response<PostLogInResponse>) {
                     var body = response!!.body()
 
-                    if(response!!.isSuccessful){  // 1. 로그인 성공
+                    if (response!!.isSuccessful) {  // 1. 로그인 성공
 
-                        if(body!!.message.equals("로그인 성공")) {
+                        if (body!!.message.equals("로그인 성공")) {
                             val token = body!!.data.token
 
                             // 그 token값을 SharedPreference에 저장.
@@ -130,12 +149,12 @@ class LoginActivity : AppCompatActivity() {
                             // todo : 이부분 어찌 인텐트 전환해줘야 할지 다시 한번 생각할 필요 있음.
                             // todo: main -> Login -> 다시 main으로 가는데 아예 새롭게 main을 호출해줘야해
                             // 로그인 했으니까 그 전화면으로 이동
-                            val intent : Intent= Intent(this@LoginActivity,MainActivity::class.java)
+                            val intent: Intent = Intent(this@LoginActivity, MainActivity::class.java)
                             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 
                             finish()
 
-                        }else{// 2. 로그인 실패( 아이디 또는 비밀번호 디비랑 맞지 않을때)
+                        } else {// 2. 로그인 실패( 아이디 또는 비밀번호 디비랑 맞지 않을때)
                             toast("아이디 또는 비밀번호가 잘못되었습니다.").setGravity(Gravity.CENTER_VERTICAL, 0, 0)
                         }
                     }
@@ -143,7 +162,7 @@ class LoginActivity : AppCompatActivity() {
                 }
 
                 override fun onFailure(call: Call<PostLogInResponse>, t: Throwable) { // 통신 실패
-                    Log.d("서버연결 실패",t.toString())
+                    Log.d("서버연결 실패", t.toString())
 
                 }
             })
--- app/src/main/java/com/wonder/bring/LoginProcess/SignupActivity.kt
+++ app/src/main/java/com/wonder/bring/LoginProcess/SignupActivity.kt
@@ -11,10 +11,11 @@ import com.wonder.bring.Network.ApplicationController
 import com.wonder.bring.Network.Get.GetCheckDuplicateIdResponseData
 import com.wonder.bring.Network.NetworkService
 import com.wonder.bring.R
+import com.wonder.bring.R.id.btn_signup_act_check_duplicate_off
+import com.wonder.bring.R.id.et_signup_act_id
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_signup.*
-import org.jetbrains.anko.startActivity
-import org.jetbrains.anko.textColor
+import org.jetbrains.anko.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,8 +23,13 @@ import retrofit2.Response
 class SignupActivity : AppCompatActivity() {
 
     var input_id: String = ""
-    var input_pw1: String = ""
-    var input_pw2: String = ""
+
+    var idCheckOk = false
+    var passEqualOk = false
+
+    lateinit var idTextListener: TextWatcher
+
+    lateinit var passTextListener: TextWatcher
 
     // 통신을 위한 변수
     val networkService: NetworkService by lazy {
@@ -34,20 +40,55 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
+        variableInit()
+        viewInit()
         setOnBtnClickListner()
 
-        // edittext 다 채워지면 , 완료버튼 색깔 변하게 하기  근데 123 중 한개라도 빠져있으면 다음 버튼 활성화 안되게 하기
-        et_signup_act_pw2.addTextChangedListener(object : TextWatcher{
+    }
+
+    private fun viewInit() {
+
+        btn_signup_act_next.isEnabled = false
+
+        et_signup_act_id.addTextChangedListener(idTextListener)
+        et_signup_act_pw1.addTextChangedListener(passTextListener)
+        et_signup_act_pw2.addTextChangedListener(passTextListener)
+    }
+
+    private fun variableInit() {
+
+        idTextListener = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
 
             override fun afterTextChanged(p0: Editable?) {
-                input_id = et_signup_act_id.text.toString()
-                input_pw1 = et_signup_act_pw1.text.toString()
-                input_pw2 = et_signup_act_pw2.text.toString()
 
-                if(input_id.length > 0 && input_pw1.length > 0 && input_pw2.length > 0)
-                    btn_signup_act_next.isSelected=true
-                else
-                    btn_signup_act_next.isSelected=false
+                input_id = et_signup_act_id.text.toString()
+
+                completeCheck()
+            }
+        }
+
+        passTextListener = object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+
+                if (et_signup_act_pw1.text.toString().equals(et_signup_act_pw2.text.toString())) {
+
+                    tv_signup_act_canusepw.visibility = View.VISIBLE
+                    tv_signup_act_canusepw.textColor = Color.BLUE
+                    tv_signup_act_canusepw.text = "비밀번호가 일치합니다."
+                    passEqualOk = true
+                } else {
+
+                    tv_signup_act_canusepw.visibility = View.VISIBLE
+                    tv_signup_act_canusepw.textColor = Color.RED
+                    tv_signup_act_canusepw.text = "비밀번호가 서로 일치하지 않습니다"
+                    passEqualOk = false
+                }
+                completeCheck()
 
             }
 
@@ -56,8 +97,22 @@ class SignupActivity : AppCompatActivity() {
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
             }
-        })
 
+        }
+    }
+
+    private fun completeCheck() {
+        if (idCheckOk && passEqualOk) {
+
+            btn_signup_act_next.isEnabled = true
+            btn_signup_act_next.backgroundResource = R.color.salmon
+            btn_signup_act_next.textColor = Color.parseColor("#FFFFFF")
+        } else {
+
+            btn_signup_act_next.isEnabled = false
+            btn_signup_act_next.backgroundResource = R.color.white
+            btn_signup_act_next.textColor = Color.parseColor("#909090")
+        }
     }
 
     private fun setOnBtnClickListner() {
@@ -82,6 +137,9 @@ class SignupActivity : AppCompatActivity() {
                                 tv_signup_act_canuseid.text = "사용 할 수 있는 아이디 입니다"
                                 tv_signup_act_canuseid.textColor = Color.BLUE
                                 Log.v("malibin debug", "중복체크 성공 : " + response.body().toString())
+                                idCheckOk = true
+
+                                completeCheck()
                             }
                             if (response.body()!!.status == 400) {
                                 tv_signup_act_canuseid.visibility = View.VISIBLE
@@ -89,16 +147,17 @@ class SignupActivity : AppCompatActivity() {
                                 tv_signup_act_canuseid.textColor = Color.RED
 
                                 Log.v("malibin debug", "중복체크 실패 : " + response.body().toString())
+                                idCheckOk = false
                             }
                         }
 
                         override fun onFailure(call: Call<GetCheckDuplicateIdResponseData>, t: Throwable) {
-                            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                            toast("서버통신에 실패하였습니다.")
                         }
 
                     })
-
             }
+
         }
 
 //             뒤로가기
@@ -108,16 +167,7 @@ class SignupActivity : AppCompatActivity() {
 
 //다음으로 넘어가는 함수 if 위에 결과값들이 다 정확하면 startactivity(nicknameactiviy)() else 정보를 정확히 입력해주세요
         btn_signup_act_next.setOnClickListener {
-
-//            패스워드가 같으면 다음 닉네임 액티비티로 이동한다!  서로 같지않으면 비밀번호가 일치하지않는다 호출
-            if (input_pw1 == input_pw2) {
-                startActivity<NicknameActivity>()
-            }
-            else {
-                tv_signup_act_canusepw.visibility = View.VISIBLE
-                tv_signup_act_canusepw.text = "비밀번호가 서로 일치하지 않습니다"
-                tv_signup_act_canusepw.textColor = Color.RED
-            }
+            toast("버튼 눌림")
         }
     }
 }
