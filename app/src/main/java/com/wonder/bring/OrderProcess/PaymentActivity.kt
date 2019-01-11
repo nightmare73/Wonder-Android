387500de16cce7195bd866e956b2401677d1e01d
Limkyuhee <kyuhee2118@gmail.com>
2019-01-12 08:51:39 +0900

    modified

--- app/src/main/java/com/wonder/bring/OrderProcess/PaymentActivity.kt
+++ app/src/main/java/com/wonder/bring/OrderProcess/PaymentActivity.kt
@@ -1,9 +1,11 @@
 package com.wonder.bring.OrderProcess
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Gravity
 import android.widget.Toast
+import com.wonder.bring.MainActivity
 import com.wonder.bring.R
 import kotlinx.android.synthetic.main.activity_payment.*
 import org.jetbrains.anko.toast
@@ -23,9 +25,6 @@ class PaymentActivity : AppCompatActivity() {
             toast("결제가 완료되었습니다.").setGravity(Gravity.CENTER_VERTICAL, 0, 0)
 
             // todo: 결제가 완료되면, 주문내역화면으로
-
-
-            finish()
 
 
 
