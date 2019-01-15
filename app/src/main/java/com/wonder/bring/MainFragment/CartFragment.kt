e3dfe113811a62f4bf932263cfb0b94336a03659
Malibin <nightmare73@naver.com>
2019-01-15 17:52:10 +0900

    cart fragment View all modified

--- app/src/main/java/com/wonder/bring/MainFragment/CartFragment.kt
+++ app/src/main/java/com/wonder/bring/MainFragment/CartFragment.kt
@@ -12,11 +12,11 @@ import com.wonder.bring.Network.ApplicationController
 import com.wonder.bring.Network.NetworkService
 
 import com.wonder.bring.R
-import com.wonder.bring.data.CartListData
+import com.wonder.bring.db.CartData
 import kotlinx.android.synthetic.main.fragment_cart.*
 
 
-class CartFragment : Fragment(){
+class CartFragment : Fragment() {
 
     companion object {
         private var instance: CartFragment? = null
@@ -39,8 +39,8 @@ class CartFragment : Fragment(){
     lateinit var cartListRecyclerViewAdapter: CartListRecyclerViewAdapter
 
 
-    val listDataList : ArrayList<CartListData> by lazy {
-        ArrayList<CartListData>()
+    val listDataList: ArrayList<CartData> by lazy {
+        ArrayList<CartData>()
     }
 
     val networkService: NetworkService by lazy {
@@ -49,7 +49,7 @@ class CartFragment : Fragment(){
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var view=inflater.inflate(R.layout.fragment_cart, container, false)
+        var view = inflater.inflate(R.layout.fragment_cart, container, false)
 
 
         return view
@@ -59,7 +59,7 @@ class CartFragment : Fragment(){
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-//        setRecyclerView()
+        setRecyclerView()
 
         btn_cart_frag_login.setOnClickListener {
 
@@ -67,46 +67,61 @@ class CartFragment : Fragment(){
         }
     }
 
-    private fun setRecyclerView(){
+    private fun setRecyclerView() {
 
-//        // 임시데이터
-//        listDataList.add(CartListData
-//            ("커피나무",
-//            "아메리카노ICE",
-//            "Regular",
-//            5000,
-//            1,
-//            "위에 생크림 올려주세요"
-//            )
-//        )
-//
-//        listDataList.add(CartListData
-//            ("커피나무",
-//            "아메리카노ICE",
-//            "Regular",
-//            5000,
-//            1,
-//            "위에 생크림 올려주세요"
-//        ))
-//
-//
-//        listDataList.add(CartListData
-//            ("커피나무",
-//            "아메리카노ICE",
-//            "Regular",
-//            5000,
-//            1,
-//            "위에 생크림 올려주세요"
-//        )
-//        )
-//
+        // 임시데이터
+        listDataList.add(
+            CartData
+                (
+                6,
+                50,
+                "https://s3.ap-northeast-2.amazonaws.com/project-bring/d3ae3b9f0f174a2d988fc25b0f4f90d0.jpg",
+                "탐앤탐스커피 역삼2호점",
+                "카페라떼HOT",
+                "위에 생크림 올려주세요",
+                2,
+                1,
+                4700
+            )
+        )
+
+        listDataList.add(
+            CartData
+                (
+                6,
+                53,
+                "https://s3.ap-northeast-2.amazonaws.com/project-bring/e2b436458b1e43faa4e9345a8e5967d2.jpg",
+                "탐앤탐스커피 역삼2호점",
+                "캬라멜마끼아또HOT",
+                "해고니가 좋아하는 캬마",
+                3,
+                2,
+                5600
+            )
+        )
+
+        listDataList.add(
+            CartData
+                (
+                6,
+                46,
+                "https://s3.ap-northeast-2.amazonaws.com/project-bring/34f56cd7156848f4aedfba20a197d528.jpg",
+                "탐앤탐스커피 역삼2호점",
+                "소이도그프레즐",
+                "간식쓰",
+                1,
+                4,
+                7300
+            )
+        )
 
 
-//        cartListRecyclerViewAdapter= CartListRecyclerViewAdapter(activity!!,listDataList)
-//        rv_cart_frag_list.adapter=cartListRecyclerViewAdapter
-//        rv_cart_frag_list.layoutManager=LinearLayoutManager(activity)
+        cartListRecyclerViewAdapter= CartListRecyclerViewAdapter(activity!!,listDataList)
+        rv_cart_frag_list.adapter=cartListRecyclerViewAdapter
+        rv_cart_frag_list.layoutManager= LinearLayoutManager(activity)
 
     }
+
     fun viewToggle(isLogin: Boolean) {
 
         if (isLogin) {
